03de9d1bef35bf23d1a461be51f98f9e700d0f6e
Nkechiyere <nwa22011@byui.edu>
2025-01-30 23:22:30 +0100

    homepage update

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,3 +5,34 @@ document.querySelector('#copyright-year').textContent = currentYear;
 // Set the last modified date in the footer
 const lastModifiedDate = document.lastModified;
 document.querySelector('#lastModified').textContent = `Last modified: ${lastModifiedDate}`;
+
+// Get the button and the menu
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('navbar');
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+menuToggle.addEventListener('click', function () {
+  menu.classList.toggle('open'); 
+  
+  // Change the button symbol (hamburger to 'X')
+  if (menu.classList.contains('open')) {
+    menuToggle.innerHTML = '&times;'; 
+  } else {
+    menuToggle.innerHTML = '&#9776;'; 
+  }
+});
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
+
